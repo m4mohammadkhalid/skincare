768b9f80f00df06cfe242ddd4a819abcb40eb9f7
m4mohammadkhalid <61066087+m4mohammadkhalid@users.noreply.github.com>
2020-12-25 23:44:27 -0800

    finalupdate

--- js/combining.js
+++ js/combining.js
@@ -1,7 +1,4 @@
-/* ========================
-	jquery.min.js
-========================= */
-/*! jQuery v1.12.4 | (c) jQuery Foundation | jquery.org/license */
+
 ! function(a, b) {
     "object" == typeof module && "object" == typeof module.exports ? module.exports = a.document ? b(a, !0) : function(a) {
         if (!a.document) throw new Error("jQuery requires a window with a document");
